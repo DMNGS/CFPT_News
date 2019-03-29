951bb5388374b401f3702eeccd64d24e6cdcbb14
stevenfavre <steven.fvr@eduge.ch>
2019-03-29 10:15:16 +0100

    ajout css

--- formulaire.php
+++ formulaire.php
@@ -12,21 +12,20 @@ $unlink = filter_input(INPUT_POST, 'unlink');
 $retour = filter_input(INPUT_POST, 'retour');
 
 if (!isset($_SESSION['nomArticleChoisi'])) {
-$nomArticle = filter_input(INPUT_POST, 'nomArticle', FILTER_SANITIZE_STRING);
-$article = filter_input(INPUT_POST, 'article', FILTER_SANITIZE_STRING);
-$choisirArticle = array();
-$articleAChoisir = filter_input(INPUT_POST, 'articleAChoisir');
-$texteModifie = filter_input(INPUT_POST, 'texteModifie', FILTER_SANITIZE_STRING);
-$nomArticleChoisi = "";
-$_SESSION['nomArticleChoisi'] = "";
-}
-else{
+    $nomArticle = filter_input(INPUT_POST, 'nomArticle', FILTER_SANITIZE_STRING);
+    $article = filter_input(INPUT_POST, 'article', FILTER_SANITIZE_STRING);
+    $choisirArticle = array();
+    $articleAChoisir = filter_input(INPUT_POST, 'articleAChoisir');
+    $texteModifie = filter_input(INPUT_POST, 'texteModifie', FILTER_SANITIZE_STRING);
+    $nomArticleChoisi = "";
+    $_SESSION['nomArticleChoisi'] = "";
+} else {
     $nomArticleChoisi = $_SESSION['nomArticleChoisi'];
-$nomArticle = filter_input(INPUT_POST, 'nomArticle', FILTER_SANITIZE_STRING);
-$article = filter_input(INPUT_POST, 'article', FILTER_SANITIZE_STRING);
-$choisirArticle = array();
-$articleAChoisir = filter_input(INPUT_POST, 'articleAChoisir');
-$texteModifie = filter_input(INPUT_POST, 'texteModifie', FILTER_SANITIZE_STRING);
+    $nomArticle = filter_input(INPUT_POST, 'nomArticle', FILTER_SANITIZE_STRING);
+    $article = filter_input(INPUT_POST, 'article', FILTER_SANITIZE_STRING);
+    $choisirArticle = array();
+    $articleAChoisir = filter_input(INPUT_POST, 'articleAChoisir');
+    $texteModifie = filter_input(INPUT_POST, 'texteModifie', FILTER_SANITIZE_STRING);
 }
 ?>
 <html>
@@ -44,66 +43,86 @@ $texteModifie = filter_input(INPUT_POST, 'texteModifie', FILTER_SANITIZE_STRING)
             fclose($fp);
         }
         ?>
+        <section style="align-content: center">
+            <form action="formulaire.php" method="POST">
+                <table>
+                    <tr>
+                        <td>
+                            <label>Créer un nom pour un nouvel article</label>
+                        </td>
+                        <td>
+                            <input type="text" name="nomArticle">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Ecrire le texte à publier</label>
+                        </td>
+                        <td>
+                            <input type="text" name="article">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input type="submit" name="ajouter" value="ajouter un nouveau article">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Choisir l'article à modifier</label>
+                        </td>
+                        <td>
+                            <?php
+                            foreach (glob("tmp/*")as $filename) {
+                                array_push($choisirArticle, $filename);
+                            }
+                            foreach ($choisirArticle as $value) {
 
-        <form action="formulaire.php" method="POST">
-
-            <p><label>Créer un nom pour un nouvel article</label><input type="text" name="nomArticle"></p>
-            <p> <label>Ecrire le texte à publier</label>
-                <input type="text" name="article"></p>    
-            <p><input type="submit" name="ajouter" value="ajouter un nouveau article"></p>
-
-            <p><label>Choisir l'article à modifier</label>
-                <!-- Afficher tout les fichiers dans le dossier tmp-->
-                <?php
-                foreach (glob("tmp/*")as $filename) {
-                    array_push($choisirArticle, $filename);
-                }
-                foreach ($choisirArticle as $value) {
-                    
-                    $value = substr($value, 4);
-                    echo "<input type='submit' name='articleAChoisir' value='$value'>";
-                }
-                if ($articleAChoisir) {
-                    $_SESSION['nomArticleChoisi'] = "";
-                    $nomArticleChoisi = $articleAChoisir;
-                    if ($_SESSION['nomArticleChoisi'] == "") {
-                        $_SESSION['nomArticleChoisi'] = $nomArticleChoisi;
-                    }
-                    ?>
-                <p><label>Voici le texte à modifier</label></p>
-                <textarea name="texteModifie"><?php echo file_get_contents("./tmp/$articleAChoisir"); ?></textarea>;
-            <?php } 
-            if ($modifier) {
-                $nomArticleChoisi = $_SESSION['nomArticleChoisi'];
-                $fp = fopen("./tmp/". $nomArticleChoisi, 'w');
-                fwrite($fp, $texteModifie);
-                fclose($fp);
-            }
-            ?>
-            <p><input type="submit" name="modifier" value="submit"></p>
-            <!-- <p><label>Supprimer un article : </label></p>-->
-            <?php
-           /* foreach ($choisirArticle as $value) {
-                    $value = substr($value, 4);
-                    echo "<input type='submit' name='unlink' value='$value'>";
-                }
-        if ($unlink) {
-            $myFileUnlink = "./tmp/".$unlink;
-            if (!unlink($myFileUnlink)) {
-                echo "Error deleting $myFileUnlink";
-            }
-            else{
-                echo "Deleted file $myFileUnlink";
-            }
-            //unlink($myFileUnlink) or die("Ne peut pas supprimer le fichier");
-        }
-           */ ?>
-            <p><label>Retourner :</label><input type="submit" name="retour" value="Retour"></p>
-        <?php
-        if ($retour) {
-            header("Location: index.php");
-        }
-        ?>
-        </form>
+                                $value = substr($value, 4);
+                                echo "<input type='submit' name='articleAChoisir' value='$value'><br>";
+                            }
+                            if ($articleAChoisir) {
+                                $_SESSION['nomArticleChoisi'] = "";
+                                $nomArticleChoisi = $articleAChoisir;
+                                if ($_SESSION['nomArticleChoisi'] == "") {
+                                    $_SESSION['nomArticleChoisi'] = $nomArticleChoisi;
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>Voici le texte à modifier</label>
+                            </td>
+                            <td>
+                                <textarea name="texteModifie"><?php echo file_get_contents("./tmp/$articleAChoisir"); ?></textarea>
+                                <?php
+                            }
+                            if ($modifier) {
+                                $nomArticleChoisi = $_SESSION['nomArticleChoisi'];
+                                $fp = fopen("./tmp/" . $nomArticleChoisi, 'w');
+                                fwrite($fp, $texteModifie);
+                                fclose($fp);
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="submit" name="modifier" value="submit">
+                        </td>
+                        <td>
+                            <label>Retourner :</label><input type="submit" name="retour" value="Retour">
+                            <?php
+                            if ($retour) {
+                                header("Location: index.php");
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </section>
     </body>
 </html>
